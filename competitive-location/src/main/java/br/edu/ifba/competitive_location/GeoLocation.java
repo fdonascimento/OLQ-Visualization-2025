8d14ba398e8efb89daa16d951c1d4b541c2ac5ae
Fagner Fonseca <fagneolli@gmail.com>
2018-03-22 22:16:47 -0300

    it refactors and corrects the haversine formula for calculate distance between geo locations

--- competitive-location/src/main/java/br/edu/ifba/competitive_location/GeoLocation.java
+++ competitive-location/src/main/java/br/edu/ifba/competitive_location/GeoLocation.java
@@ -19,14 +19,21 @@ public class GeoLocation {
 	}
 	
 	public Double distance(GeoLocation other) {
-	    double latDistance = Math.toRadians(other.latitude - this.latitude);
+		final double earthRadius = 6371; //Km
+		
+		double startLatitude = Math.toRadians(this.latitude);
+		double endLatitude = Math.toRadians(other.latitude);
+	    double latDistance = endLatitude - startLatitude;
 	    double lngDistance = Math.toRadians(other.longitude - this.longitude);
 
-	    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-	      + Math.cos(Math.toRadians(other.latitude)) * Math.cos(Math.toRadians(this.latitude))
-	      * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
-
-	    return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    double a = haversin(latDistance) + Math.cos(startLatitude) * Math.cos(endLatitude) * haversin(lngDistance);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    return c * earthRadius;
+	}
+	
+	private double haversin(double distanceInRadians) {
+		double sin = Math.sin(distanceInRadians / 2);
+		return Math.pow(sin, 2);
 	}
 
 }
