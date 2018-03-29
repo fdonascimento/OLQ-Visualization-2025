4bc1ccc5292e92d89047c5e5fed3f78275ae9bd7
Fagner Fonseca <fagneolli@gmail.com>
2018-03-29 13:53:34 -0300

    it refactors method run

--- competitive-location/src/main/java/br/edu/ifba/competitive_location/CompetitiveLocationQuery.java
+++ competitive-location/src/main/java/br/edu/ifba/competitive_location/CompetitiveLocationQuery.java
@@ -5,16 +5,25 @@ public class CompetitiveLocationQuery {
 	public Candidate run(Clients clients, Facilities facilities, CandidateLocations candidates) {
 		Candidate bestCandidate = null;
 		for (Candidate candidate : candidates) {
-			candidate.startScore();
-			for (Client client : clients) {
-				calculateCandidateScore(facilities, client, candidate);
-			}
-			if (bestCandidate == null || candidate.score() > bestCandidate.score()) {
-				bestCandidate = candidate;
-			}
+			calculateCandidateScore(clients, facilities, candidate);
+			bestCandidate = getBestCandidate(bestCandidate, candidate);
 		}
 		
 		return bestCandidate;
+	}
+
+	private Candidate getBestCandidate(Candidate bestCandidate, Candidate newCandidate) {
+		if (bestCandidate == null || newCandidate.score() > bestCandidate.score()) {
+			bestCandidate = newCandidate;
+		}
+		return bestCandidate;
+	}
+
+	private void calculateCandidateScore(Clients clients, Facilities facilities, Candidate candidate) {
+		candidate.startScore();
+		for (Client client : clients) {
+			calculateCandidateScore(facilities, client, candidate);
+		}
 	}
 	
 	private void calculateCandidateScore(Facilities facilities, Client client, Candidate candidate) {
