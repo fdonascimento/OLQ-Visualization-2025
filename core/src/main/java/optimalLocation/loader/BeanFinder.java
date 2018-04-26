ea88328ee702ccb7d16f527d1941a63e8ea2435c
Fagner Fonseca <fagneolli@gmail.com>
2018-04-25 23:53:47 -0300

    Refactor depency class loader and implement finders for all dependencies

--- core/src/main/java/optimalLocation/loader/BeanFinder.java
+++ core/src/main/java/optimalLocation/loader/BeanFinder.java
@@ -12,34 +12,72 @@ import org.springframework.context.annotation.ClassPathScanningCandidateComponen
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.type.filter.AssignableTypeFilter;
 
-import optimalLocaiton.query.LocationQuery;
 import optimalLocation.configuration.FolderSettings;
+import optimalLocation.query.LocationQuery;
+import optimalLocation.query.providers.CandidateProvider;
+import optimalLocation.query.providers.ClientProvider;
+import optimalLocation.query.providers.FacilityProvider;
 
-public class ClassLoader {
+public class BeanFinder {
 
 	public static LocationQuery findLocationQuery(String jarName) throws FindLocationQueryException {
-		Path path = Paths.get(System.getProperty("user.dir"), FolderSettings.LOCATION_QUERY, String.format("%s.jar", jarName));
+		Path jarPath = Paths.get(System.getProperty("user.dir"), FolderSettings.LOCATION_QUERY, String.format("%s.jar", jarName));
+		return findClass(LocationQuery.class, jarPath);
+	}
+	
+	public static FacilityProvider findFacilityProvider(String jarName) throws FindLocationQueryException {
+		Path jarPath = Paths.get(System.getProperty("user.dir"), FolderSettings.FACILITY_PROVIDERS, String.format("%s.jar", jarName));
+		return findClass(FacilityProvider.class, jarPath);
+	}
+	
+	public static CandidateProvider findCandidateProvider(String jarName) throws FindLocationQueryException {
+		Path jarPath = Paths.get(System.getProperty("user.dir"), FolderSettings.CANDIDATE_PROVIDERS, String.format("%s.jar", jarName));
+		return findClass(CandidateProvider.class, jarPath);
+	}
+	
+	public static ClientProvider findClientProvider(String jarName) throws FindLocationQueryException {
+		Path jarPath = Paths.get(System.getProperty("user.dir"), FolderSettings.CLIENT_PROVIDERS, String.format("%s.jar", jarName));
+		return findClass(ClientProvider.class, jarPath);
+	}
+
+	private static <T> T findClass(Class<T> classToFind, Path jarPath) throws FindLocationQueryException {
+		URLClassLoader classLoader = createUrlClassLoader(jarPath);
+		Optional<BeanDefinition> optionalBean = getBeanDefinition(classToFind, classLoader);
+		
+		String errorMessage = "Was not found a class that implements the %s interface.";
+		String className = classToFind.getSimpleName();
+		if (!optionalBean.isPresent()) throw new FindLocationQueryException(String.format(errorMessage, className));
+		
+		return instantiateBean(classLoader, optionalBean);
+	}
+
+	private static URLClassLoader createUrlClassLoader(Path jarPath) throws FindLocationQueryException {
 		URL[] urls = null;
 		try {
-			urls= new URL[]{path.toUri().toURL()};
+			urls= new URL[]{jarPath.toUri().toURL()};
 		} catch (MalformedURLException e) {
 			throw new FindLocationQueryException("Error when trying to generate the url to read the jar.", e);
 		}
-		URLClassLoader classLoader = new URLClassLoader(urls, ClassLoader.class.getClassLoader());
 		
-		ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
-		provider.addIncludeFilter(new AssignableTypeFilter(LocationQuery.class));
-		provider.setResourceLoader(new PathMatchingResourcePatternResolver(classLoader));
-		Optional<BeanDefinition> optionalBean = provider.findCandidateComponents("").stream().findFirst();
-		
-		if (!optionalBean.isPresent()) throw new FindLocationQueryException("Was not found a class that implements the LocationQuery interface.");
-		
+		URLClassLoader classLoader = new URLClassLoader(urls, BeanFinder.class.getClassLoader());
+		return classLoader;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> T instantiateBean(URLClassLoader classLoader, Optional<BeanDefinition> optionalBean) throws FindLocationQueryException {
 		String className = optionalBean.get().getBeanClassName();
 		try {
-			return (LocationQuery) Class.forName(className, true, classLoader).newInstance();
+			return (T) Class.forName(className, true, classLoader).newInstance();
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 			String message = "Error when trying to instantiate the class: " + className;
 			throw new FindLocationQueryException(message, e);
 		}
+	}
+
+	private static <T> Optional<BeanDefinition> getBeanDefinition(Class<T> classToFind, URLClassLoader classLoader) {
+		ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
+		provider.addIncludeFilter(new AssignableTypeFilter(classToFind));
+		provider.setResourceLoader(new PathMatchingResourcePatternResolver(classLoader));
+		return provider.findCandidateComponents("").stream().findFirst();
 	}
 }
