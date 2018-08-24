6548b2e59a563b7eb86a6b7601e14f2957ba3df4
Fagner Fonseca <fagneolli@gmail.com>
2018-08-23 23:06:18 -0300

    Allow the CORS
    CORS - Cross-Origin resource sharing

--- locationQuery-rest/src/main/java/optimalLocation/locationQuery_rest/OptimalLocationQueryController.java
+++ locationQuery-rest/src/main/java/optimalLocation/locationQuery_rest/OptimalLocationQueryController.java
@@ -3,9 +3,11 @@ package optimalLocation.locationQuery_rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import optimalLocation.core.OptimalLocationQuery;
@@ -27,12 +29,14 @@ public class OptimalLocationQueryController {
 		this.inputQuery = new InputQueryJson();
 	}
 	
+	@CrossOrigin(methods=RequestMethod.POST)
 	@PostMapping("/input-candidates")
 	public ResponseEntity<String> test(@RequestBody InputQueryJson inputQuery) {
 		this.inputQuery = inputQuery;
 		return ResponseEntity.status(HttpStatus.ACCEPTED).build();
 	}
 	
+	@CrossOrigin(methods=RequestMethod.GET)
 	@GetMapping("/findBestLocation")
 	public LocationQueryResultJson findBestLocation() {
 		CandidateConverter converter = new CandidateConverter();
