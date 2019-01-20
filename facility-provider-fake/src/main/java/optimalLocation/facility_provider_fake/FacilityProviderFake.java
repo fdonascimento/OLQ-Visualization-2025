977bf33f5ef75fdfc6fe1ca8d4bf3e033326d158
Fagner Fonseca <fagneolli@gmail.com>
2019-01-20 18:47:47 -0300

    add more facilities in FacilityProviderFake

--- facility-provider-fake/src/main/java/optimalLocation/facility_provider_fake/FacilityProviderFake.java
+++ facility-provider-fake/src/main/java/optimalLocation/facility_provider_fake/FacilityProviderFake.java
@@ -10,6 +10,9 @@ public class FacilityProviderFake implements FacilityProvider
 		Facilities facilities = new Facilities();
 		facilities.addFacility(-12.992953, -38.5152408, "Garcia");
 		facilities.addFacility(-12.9232927, -38.505632, "Bonfim");
+		facilities.addFacility(-12.960991, -38.467068, "Cabula");
+		facilities.addFacility(-12.903771, -38.405956, "Cajazeiras");
+		facilities.addFacility(-12.953924, -38.404452, "Patamares");
 		return facilities;
 	}
 }
